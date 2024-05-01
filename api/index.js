20c58fff381b05bf8b569a42720ffad305140053
akash <asassss337@gmail.com>
2024-05-01 17:02:14 +0530

    connected to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,20 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Connection error:", err);
+  });
 
 const app = express();
 
 app.listen(3001, () => {
-  console.log("server running on port 3001");
+  console.log("Server running on port 3001");
 });
